2f9bf2a6cc076f2005aefca1654e00fc825cec91
vikas-go <lprogramming483@gmail.com>
2025-01-06 22:39:11 +0530

    Add files via upload

---
+++ sigma web dev/javascript/1.variable & data type.js
@@ -0,0 +1,52 @@
+// variable & data type 
+
+
+// 1.number & 2.string type
+
+// a = 1
+// a = "vikas"
+
+// a = "vikas"
+
+// console.log(a);
+// v = typeof(a);
+// console.log(v);
+
+
+// Note: js me variable ek name ke nhi rakhte
+
+// 3.boolean type
+
+// let x = true;
+// let y = 1.4;
+
+// console.log(x,y);
+
+// v = typeof (x,y);
+
+// console.log(v);
+
+
+
+
+
+
+
+// let val = parseFloat('2.3') + parseFloat('2.4');
+// console.log("2.3 + 2.4 = " + val);
+
+// function gfg_Run() {
+//     console.log("2.3 + 2.4 = "
+//         + (parseFloat('2.3') +
+//             parseFloat('2.4')).toFixed(2));
+// }
+// gfg_Run()
+
+
+function myFunction(p1, p2) {
+   return p1 * p2;
+ }
+ 
+ let result = myFunction(4, 3);
+ document.getElementById("demo").innerHTML = result;
+ 
---
+++ sigma web dev/javascript/tryle.js
@@ -0,0 +1,83 @@
+// 3 type ke loop , 1.for loop , 2.while loop , 3.do-while loop
+
+// 1.for loop
+
+// a = 12; 
+// b = 13;
+// console.log(a+b);
+// console.log('Hello World');
+
+// for (i = 1; i <= 5; i++){
+//    console.log(i,"vikas");
+// }
+
+// for(let i = 10; i>=0; i--){
+//    console.log(i,"vikas");
+   
+// }
+
+
+
+// 2.while loop
+
+// let i = 1;
+// while(i<=4){
+//    console.log(i,"kartik");
+//    i++
+// }
+
+
+
+// let i = 1;
+// while(i >= 8){
+//    console.log(i,"vikas");
+//    i--
+// } 
+
+
+// 3.do-while loop
+
+// let i = 1;
+// do{
+//    console.log(i,"vikas")
+
+//    i++
+// } while(i<=10);
+
+    
+
+// let i = 10;
+
+// do{
+//    console.log(i,"vikas")
+//    i--
+
+// }while(i>0);
+
+
+// for(let i = 10; i<=10; i++){
+
+//    console.log(i,"vikas")
+// }
+
+     
+
+// let i = 1;
+// do{
+//    console.log(i,"vikas")
+//    i++
+// }while(i<=0)
+
+
+
+// let i = 1;
+// while(i<=1){
+//    console.log(i,"vikas");
+//    i++
+// }
+
+function my(p1,p2){
+   return(p1+p2);
+}
+
+let v = my;
